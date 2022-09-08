f8b1a4de203389b7c54a62b9080c46d64b66d4d7
GillFillget <bretm1002@ksu.edu>
2022-09-08 14:32:30 -0500

    need to finish parse command

--- jsonreadtest.js
+++ jsonreadtest.js
@@ -1,4 +1,5 @@
 let fs = require('fs');
 fs.readFile("lizardJSON.json",() => {})
 
-let lizard = JSON.parse(fs.readFile("lizardJSON.json",() => {}))
+//let lizard = JSON.parse(fs.readFile("lizardJSON.json",() => {}))
+//"\Users\Gill\Documents\Github\Programming\JSONpractice\lizardJSON.json"
