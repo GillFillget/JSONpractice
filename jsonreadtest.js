232c5733c88b4cf0c8b888458daaae10ec8825ef
GillFillget <bretm1002@ksu.edu>
2022-09-12 20:59:19 -0500

    Created the ability to read json files

--- jsonreadtest.js
+++ jsonreadtest.js
@@ -1,5 +1,17 @@
-let fs = require('fs');
-fs.readFile("lizardJSON.json",() => {})
+const fs = require('fs');
 
-//let lizard = JSON.parse(fs.readFile("lizardJSON.json",() => {}))
-//"\Users\Gill\Documents\Github\Programming\JSONpractice\lizardJSON.json"
+fs.readFile('./lizardJSON.json', 'utf8', (err, jsonString) => {
+    if (err) {
+        console.log("Error reading file from disk:", err);
+        return;
+    }
+    try {
+        const animal = JSON.parse(jsonString)
+        console.log("The name of the lizard is " + animal.animalName);
+        console.log("The sound of the lizard is " + animal.animalSound);
+        console.log("The age of the lizard is " + animal.animalAge);
+        console.log("The color of the lizard is " + animal.animalColor);
+} catch(err) {
+        console.log('Error parsing JSON string:', err);
+    }
+})
